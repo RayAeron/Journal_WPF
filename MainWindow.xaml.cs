334caae8a4c0c5dc11f9d76a5e35a17c01d5ecf8
Ray Aeron <rayaeron95@hotmail.com>
2021-04-28 03:28:07 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace Journal_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void pass_recovery_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            //reck_pass reck_pass = new reck_pass();
+            //reck_pass.Show();
+            //this.Close();
+        }
+
+    }
+}
